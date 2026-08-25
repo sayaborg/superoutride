b384734518ebe70588a8f4a4c5bb28a7c2779bc1
sayaborg <hara.kazutaka.zzz@gmail.com>
2026-08-25 22:01:02 +0900

    Update M6.6 debug-course authority path

--- tests/m6-6-input-trace.test.mjs
+++ tests/m6-6-input-trace.test.mjs
@@ -2,7 +2,7 @@ import assert from 'node:assert/strict';
 import test from 'node:test';
 
 import { SIM_DT } from '../dist/core/constants.js';
-import { createM2StadiumGuide } from '../dist/core/debug-course.js';
+import { createM2StadiumGuide } from '../dist/dev/debug-course.js';
 import {
   appendDrivingInput,
   createDrivingInputTrace,
